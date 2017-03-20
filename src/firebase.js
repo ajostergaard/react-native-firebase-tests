8ff388080bcf163465d8bfc9d614058aee7719fc
Aleck Greenham <greenhama13@gmail.com>
2017-03-20 07:12:50 +0000

    Extract database types into separate file for re-use

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
 import RNfirebase from 'react-native-firebase';
 
+import databaseContents from './tests/support/databaseContents';
+
 const config = {
   apiKey: "AIzaSyDnVqNhxU0Biit9nCo4RorAh5ulQQwko3E",
   authDomain: "rnfirebase-b9ad4.firebaseapp.com",
@@ -18,17 +20,7 @@ const instances = {
   }),
 };
 
-instances.web.database().ref('tests/types').set({
-  array: [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-  ],
-  boolean: true,
-  string: 'foobar',
-  number: 123567890,
-  object: {
-    foo: 'bar',
-  }
-});
+instances.web.database().ref('tests/types').set(databaseContents);
 
 instances.web.database().ref('tests/priority').setWithPriority({
   foo: 'bar',
---
+++ src/tests/support/databaseContents.js
@@ -0,0 +1,11 @@
+export default {
+  array: [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  ],
+  boolean: true,
+  string: 'foobar',
+  number: 123567890,
+  object: {
+    foo: 'bar',
+  }
+};
